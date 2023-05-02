37647849c97bf638e01d17fa5d557bb44e7e3458
Nrezhang <102526155+Nrezhang@users.noreply.github.com>
2023-05-02 22:08:02 +0800

    bootstrap

--- students/models.py
+++ students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+        id = models.AutoField(primary_key=True)
+        name = models.CharField(max_length=100)
+        email = models.EmailField(max_length=100)
+        major = models.CharField(max_length=100)
+        gpa = models.FloatField()
+def __str__(self):
+        return self.name
+
--- students/views.py
+++ students/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import student
 
 # Create your views here.
+def index(request):
+    return render(request, 'students/index.html', 
+{'students': student.objects.all()})
+
